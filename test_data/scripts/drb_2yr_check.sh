92eb2952183f0151d148ed96e3dd6aeb0890dc8e
James McCreight <jamesmcc@ucar.edu>
2022-02-16 17:09:07 -0700

    update drb_2yr with output

--- test_data/scripts/drb_2yr_check.sh
+++ test_data/scripts/drb_2yr_check.sh
@@ -1,60 +1,100 @@
 #!/bin/bash
 
+# config
 source_dir=/Users/jamesmcc/usgs/data/prms_nhm_applications/drb
 target_dir=/Users/jamesmcc/usgs/data/prms_nhm_applications/drb_2yr
-
 run_dir=/Users/jamesmcc/usgs/data/prms_nhm_applications/drb_2yr_check
+repo_dir=/Users/jamesmcc/usgs/prmsNHMpy
+prms=/Users/jamesmcc/usgs/prmsNHMpy/prms5.2.1/bin/prms
+
+# ---------------------------------
+
 source_run_dir=$run_dir/run_drb
 target_run_dir=$run_dir/run_drb_2yr
+# The control file for this is under version control
+control_file=$repo_dir/test_data/drb_2yr/control.test
+
 rm -rf $source_run_dir
 rm -rf $source_target_dir
-mkdir -p $source_run_dir
-mkdir -p $target_run_dir
-
-prms=/Users/jamesmcc/usgs/prmsNHMpy/prms5.2.1/bin/prms
+mkdir -p $source_run_dir/output
+mkdir -p $target_run_dir/output
 
 
 # The OG
 cd $source_run_dir
 cp $prms .
-for ii in control.default myparam.param sf_data *cbh
+for ii in myparam.param sf_data *cbh
 do
     cp $source_dir/$ii .
 done
-# change the start and end dates by line number
-# change the output option
-# osx specific sed
-sed -i '' "6s/.*/1979/" control.default
-sed -i '' "7s/.*/1/" control.default
-sed -i '' "16s/.*/1981/" control.default
-sed -i '' "17s/.*/1/" control.default
-sed -i '' "136s/.*/2/" control.default
-./prms control.default -set param_file ./myparam.param
+cp $control_file .
+./prms control.test -set param_file ./myparam.param
 
 
 # The Subset
 cd $target_run_dir
 cp $prms .
-for ii in control.default myparam.param sf_data *cbh
+for ii in myparam.param sf_data *cbh
 do
-    cp $source_dir/$ii .
+    cp $target_dir/$ii .
 done
-# change the start and end dates by line number
-# change the output option
-# osx specific sed
-sed -i '' "6s/.*/1979/" control.default
-sed -i '' "7s/.*/1/" control.default
-sed -i '' "16s/.*/1981/" control.default
-sed -i '' "17s/.*/1/" control.default
-sed -i '' "136s/.*/2/" control.default
-./prms control.default -set param_file ./myparam.param
+cp $control_file .
+./prms control.test -set param_file ./myparam.param
 
-result=`\diff $source_run_dir/stats.csv $target_run_dir/stats.csv`
+# Checks
+diff_files () {
+    file1=$1
+    file2=$2
+    if [[ $file1 == $file2 ]];
+    then
+	echo
+	echo "Diffing the same file: $file1 == $file2"
+	echo
+	return 1
+    fi
+    ls $file1 > /dev/null 2>&1 || return 1
+    ls $file2 > /dev/null 2>&1 || return 1
+    \diff $file1 $file2
+    result=$?
+    if [[ $result -eq 0 ]]
+    then
+	echo success: `basename $file1`
+    else
+	echo FAILURE: `basename $file1`
+    fi
+    return $result
+}
 
-if [[ $result -eq 0 ]]; then
-    echo 'Success'
-else
-    echo 'Failure'
-fi
+for ff in $target_run_dir/output/*
+do
+    ff2=$source_run_dir/output/$(basename $ff)
+    diff_files $ff $ff2
+    result=$?
+    if [[ $result -ne 0 ]]
+    then
+	exit 1
+    fi
+done
 
-exit $result
+controlled_files=(
+    control.test \
+    myparam.param \
+    prcp.cbh \
+    rhavg.cbh \
+    tmax.cbh \
+    tmin.cbh \
+    output/nhru_tmaxf.csv \
+    output/nhru_tminf.csv \
+    output/nhru_hru_ppt.csv \
+    output/nhru_hru_rain.csv \
+    output/nhru_hru_snow.csv )
+
+echo "You can optionally copy the version controlled files "
+echo "to the repo using the following commands: "
+for ff in "${controlled_files[@]}"
+do
+    echo "cp $target_run_dir/$ff $repo_dir/test_data/drb_2yr/$ff"
+done
+
+
+exit 0

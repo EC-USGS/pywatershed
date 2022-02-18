2bf5f62b00ac3efe63686e090debd7cfef6cee7b
James McCreight <jamesmcc@ucar.edu>
2022-02-18 10:44:12 -0700

    Ci coverage (#8)
    * ci_coverage 2
    
    * ci_coverage 3
    
    * ci_coverage 4
    
    * *replace cbh_read with preprocess/cbh
    *clean up tests to use drb_2yr
    
    * ci_coverage 5
    
    * ci coverage 6
    
    * ci coverage 7
    
    * add requirements.txt for ci
    
    * ci coverage 8
    
    * ci coverage 9
    
    * ci coverage 10
    
    * ci coverage 11

--- pynhm/__init__.py
+++ pynhm/__init__.py
@@ -8,7 +8,6 @@ from .prms5util import (
     load_prms_statscsv,
     load_wbl_output,
 )
-from .prms5util_cbh import CbhInput
 from .prmsCanopy import prmsCanopy
 from .prmsSurfaceRunoff import prmsSurfaceRunoff
 from .pynhm import driver

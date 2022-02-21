20e3f8b19ba7ab226179f248d82b155325c4fecf
Joseph D Hughes <jdhughes@usgs.gov>
2022-02-21 15:34:05 -0600

    feat: add Forcings and CSVFile classes
    isort import and black formatting. Add CSVFile test. Added setup.cfg
    and version.py files.

--- pynhm/__init__.py
+++ pynhm/__init__.py
@@ -1,5 +1,3 @@
-from version import __author__, __author_email__, __version__
-
 from .atmosphericForcings import AtmForcingsNHM, AtmosphericForcings
 from .parameters import PrmsParameters
 from .preprocess.csv_utils import CsvFile
@@ -7,3 +5,4 @@ from .prms5util import load_prms_output, load_prms_statscsv, load_wbl_output
 from .prmsCanopy import prmsCanopy
 from .prmsSurfaceRunoff import prmsSurfaceRunoff
 from .pynhm import driver
+from .version import __author__, __author_email__, __version__

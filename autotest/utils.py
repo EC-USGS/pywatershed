69e837061fc1571cf7048120647b3cbbe687c68a
James McCreight <jamesmcc@ucar.edu>
2022-02-28 13:20:27 -0700

    test utils and box01.yaml

---
+++ autotest/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+print_ans = False
+
+
+def assert_or_print(results, answers, test_name=None, print_ans=False):
+    print("\n")
+    if test_name is not None:
+        test_name = f"{test_name}:"
+    n_space = 4
+    if print_ans and (test_name is not None):
+        sp = "".join(n_space * [" "])
+        n_space = n_space + 2
+        print(f"{sp}{test_name}")
+
+    for key in results.keys():
+        if print_ans:
+            sp = "".join(n_space * [" "])
+            print(f"{sp}{key}: {results[key]}")
+        else:
+            msg = f"{test_name}{key}"
+            assert np.isclose(results[key], answers[key]), msg
+    # always fail if printing answers
+    assert not print_ans
+    return

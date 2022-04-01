5e26dde17fe8eed18bcbb85fa20372fef1ae2d85
James McCreight <jmccreight@ucar.edu>
2022-04-01 16:29:12 -0500

    Changes for CONUS testing synched with repo

---
+++ test_data/README.md
@@ -0,0 +1,27 @@
+# test_data/
+
+This directory contains
+  * domain directories for domain tests
+  * scripts/ which document how domain tests are established.
+
+
+## Domain directories
+
+Some domain directories only contain `control.test` and `domain.yaml` files. These domains are not
+to be run in a self-contained way within the repository. They rely on other files on other
+systems. The main example is `conus_2yr/`. It is to be run on Denali/Tallgrass and its
+input files are too large to be included in this repo. However, we do want to version control
+the files in the directory. See the `conus_2yr/README.md` for details on how it is setup.
+
+
+Other domain directories can be run from pytest given the repository. The pytest flag `--all_domains`
+will detect these repos and run them.
+
+
+
+# scripts/
+
+This contains code for reproducing test data in the domains. Importantly, `run_domains.py` should be run
+occasionally to update the test data.
+
+Original source data can be found on Denali for most (all?) domains in `/home/jmccreight/pynhm_data`.
--- test_data/scripts/run_domains.py
+++ test_data/scripts/run_domains.py
@@ -1,6 +1,10 @@
+# This requires flopy to be installed
+# which is not in the requirements.
+
 import os
 import pathlib as pl
 import sys
+from fnmatch import fnmatch
 
 import pytest
 from flopy import run_model
@@ -13,22 +17,56 @@ elif platform == "darwin":
     exe_name += "_mac"
 elif platform == "linux":
     exe_name += "_linux"
-exe_pth = pl.Path(f"../../bin/{exe_name}")
+exe_pth = pl.Path(f"../../bin/{exe_name}").resolve()
 
 
 rootdir = ".."
-test_dirs = [path for path in pl.Path(rootdir).iterdir() if path.is_dir()]
+test_dirs = sorted(
+    [path for path in pl.Path(rootdir).iterdir() if path.is_dir()]
+)
+
+# This would change to handle other/additional schedulers
+domain_globs_schedule = {"*conus*": "slurm"}
+
+
+def is_scheduled(scheduler):
+    if scheduler == "slurm":
+        return os.getenv("SLURM_JOB_ID") is not None
+    # add more scheduler checks here
+
 
 simulations = {}
 for test_dir in test_dirs:
+
+    # If the test_dir is required to be scheduled, check if we are
+    # running in the scheduler
+    doms_sched = list(domain_globs_schedule.keys())
+    glob_match = list(fnmatch(str(test_dir), kk) for kk in doms_sched)
+    if any(glob_match):
+        the_glob = doms_sched[glob_match.index(True)]
+        scheduler = domain_globs_schedule[the_glob]
+        if not is_scheduled(scheduler):
+            raise RuntimeError(
+                f"Domain '{test_dir}' must be scheduled using {scheduler}"
+            )
+
     for pth in test_dir.iterdir():
-        if pth.is_file() and pth.name == "control.test":
+        # checking for prcp.cbh ensure this is a self-contained run (all files in repo)
+        if (
+            (test_dir / "prcp.cbh").exists()
+            and pth.is_file()
+            and pth.name == "control.test"
+        ):
             # add simulation
             simulations[str(test_dir)] = pth.name
 
             # create output directory if it doesn't exist
+            # JLM: should we force remove existing outputs?
             output_dir = pl.Path(test_dir) / "output"
             output_dir.mkdir(parents=True, exist_ok=True)
+
+print("\nrun_domains.py found the following domains to run:\n")
+print(f"{list(simulations.keys())}")
 
 
 def test_exe_available():
@@ -43,14 +81,21 @@ def test_exe_available():
     ids=simulations.keys(),
 )
 def test_prms_run(ws, control_file):
-    print(f"running '{control_file}' in {ws}")
+    print(f"\n\n\n{'*' * 70}\n{'*' * 70}")
+    print(f"run_domains.py: Running '{control_file}' in {ws}\n\n", flush=True)
     success, buff = run_model(
         exe_pth,
         control_file,
         model_ws=ws,
+        cargs=[
+            "-MAXDATALNLEN",
+            "60000",
+        ],
         normal_msg="Normal completion of PRMS",
     )
     assert success, f"could not run prms model in '{ws}'"
+
+    print(f"run_domains.py: End of domain {ws}\n", flush=True)
 
 
 if __name__ == "__main__":

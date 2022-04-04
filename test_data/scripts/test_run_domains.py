de2bfe4fa4466f517e5ebf8df8aa5a58d3d7ba9f
James McCreight <jamesmcc@ucar.edu>
2022-04-04 16:14:29 -0600

    exist_ok=False

--- test_data/scripts/test_run_domains.py
+++ test_data/scripts/test_run_domains.py
@@ -29,7 +29,7 @@ def test_prms_run(simulation, exe):
     output_dir = pl.Path(ws) / "output"
     if output_dir.exists():
         shutil.rmtree(output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
+    output_dir.mkdir(parents=True)
 
     success, buff = run_model(
         exe,

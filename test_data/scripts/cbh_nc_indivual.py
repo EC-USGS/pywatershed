a98013d021183eb13ed5a21447da1445e9c65da9
James McCreight <jamesmcc@ucar.edu>
2022-06-29 11:26:47 -0600

    separate cbh netcdf inputs

---
+++ test_data/scripts/cbh_nc_indivual.py
@@ -0,0 +1,22 @@
+import pathlib as pl
+import xarray as xr
+
+domains = ["drb_2yr", "ucb_2yr", "hru_1"]
+
+cbh_files = [pl.Path(f"../{domain}/cbh.nc") for domain in domains]
+
+vars = ["prcp", "tmin", "tmax"]
+
+for cbh_file in cbh_files:
+    cbh = (
+        xr.open_dataset(cbh_file)
+        .swap_dims({"hru": "nhm_id"})
+        .set_coords("datetime")
+    )
+
+    for var in vars:
+        cbh_var = cbh[var]
+        cbh_var.to_netcdf(cbh_file.parent / f"{var}.nc")
+
+
+print("")

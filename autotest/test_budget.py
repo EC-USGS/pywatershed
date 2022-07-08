8842cab4e9808077472a5c13735abb457fea3e52
James McCreight <jamesmcc@ucar.edu>
2022-07-08 18:07:13 -0400

    suppress expected warning

--- autotest/test_budget.py
+++ autotest/test_budget.py
@@ -22,6 +22,7 @@ def control_simple():
     return Control(**time_dict)
 
 
+@pytest.mark.filterwarnings("ignore:Metadata unavailable")
 def test_budget(control_simple):
     nhru = 5
     inputs = {

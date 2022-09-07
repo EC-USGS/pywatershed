280298b8bf2ccac683d85a2b3ccf83a5c79fe67b
James McCreight <jamesmcc@ucar.edu>
2022-09-07 17:37:52 -0600

    ci for numbered notebook examples

---
+++ autotest_exs/test_notebooks.py
@@ -0,0 +1,40 @@
+import pathlib as pl
+import subprocess
+import sys
+
+import pytest
+from pynhm.constants import __pynhm_root__
+
+# "Official" notebooks are numbered
+notebooks = sorted(
+    pl.Path(__pynhm_root__).parent.joinpath("examples").glob("[0-9]*.ipynb")
+)
+
+
+@pytest.mark.parametrize("notebook", notebooks)
+def test_notebooks(notebook):
+
+    # Convert the notebook to a .py version of itself using jupyter nbconvert
+    # this formats magics in a way that ipython can run
+    cmd = ["jupyter", "nbconvert", "--to", "script", str(notebook)]
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    assert (
+        proc.returncode == 0
+    ), f"Failed to convert notebook to script: {notebook}"
+    nb_py = notebook.with_suffix(".py")
+    assert nb_py.exists(), f"Expected script does not exists: {nb_py}"
+
+    bin_path = pl.Path(sys.executable).parent
+    ipython = bin_path / "ipython"
+    assert ipython.exists(), f"ipython not found at: {ipython}"
+
+    cmd = ["ipython", str(nb_py)]
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    msg = (
+        f"Running the notebook failed with stdout: "
+        "{proc.stdout.decode('utf-8')}"
+    )
+    assert proc.returncode == 0, msg
+
+    nb_py.unlink()
+    assert not nb_py.exists(), "Problem removing {nb_py}"

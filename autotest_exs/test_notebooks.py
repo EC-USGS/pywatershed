2510746aff785423fec16b42ec82fd7988a1ff69
James McCreight <jamesmcc@ucar.edu>
2022-09-07 21:46:22 -0600

    another ci attempt

--- autotest_exs/test_notebooks.py
+++ autotest_exs/test_notebooks.py
@@ -19,8 +19,8 @@ def test_notebooks(notebook):
     # Convert the notebook to a .py version of itself using jupyter nbconvert
     # this formats magics in a way that ipython can run
     cmd = [
-        "python",
-        "-m",
+        # "python",
+        # "-m",
         "jupyter",
         "nbconvert",
         "--to",

6cd8c5999a3a419e217e53087708d04570dfe558
James McCreight <jamesmcc@ucar.edu>
2022-10-20 11:04:40 -0600

    canopy with numba only reducing runtime by 75% on ucb_2yr (20s -> 5s)

--- pynhm/hydrology/PRMSCanopy.py
+++ pynhm/hydrology/PRMSCanopy.py
@@ -1,3 +1,4 @@
+import numba as nb
 import numpy as np
 
 from ..base.adapter import adaptable
@@ -49,7 +50,6 @@ class PRMSCanopy(StorageUnit):
         """
         return (
             "nhru",
-            "hru_area",
             "cov_type",
             "covden_sum",
             "covden_win",
@@ -139,7 +139,45 @@ class PRMSCanopy(StorageUnit):
             # todo: not implemented yet
             self.calculate_vectorized(time_length)
         else:
-            self.calculate_procedural(time_length)
+
+            (
+                self.intcp_evap,
+                self.intcp_stor,
+                self.net_rain,
+                self.net_snow,
+                self.net_ppt,
+                self.hru_intcpstor,
+                self.hru_intcpevap,
+                self.intcp_changeover,
+                self.intcp_transp_on,
+            ) = self._calculate_procedural(
+                self.cov_type,
+                self.covden_sum,
+                self.covden_win,
+                self.hru_intcpstor,
+                self.hru_intcpevap,
+                self.hru_ppt,
+                self.hru_rain,
+                self.hru_snow,
+                self.intcp_changeover,
+                self.intcp_evap,
+                self.intcp_form,
+                self.intcp_stor,
+                self.intcp_transp_on,
+                self.net_ppt,
+                self.net_rain,
+                self.net_snow,
+                self.nhru,
+                self.pkwater_ante,
+                self.potet,
+                self.potet_sublim,
+                self.snow_intcp,
+                self.srain_intcp,
+                self.transp_on,
+                self.wrain_intcp,
+                time_length,
+                self._intercept,
+            )
 
         self.hru_intcpstor_change[:] = (
             self.hru_intcpstor - self.hru_intcpstor_old
@@ -147,96 +185,124 @@ class PRMSCanopy(StorageUnit):
 
         return
 
-    def calculate_procedural(self, time_length):
+    @staticmethod
+    @nb.jit(nopython=True)
+    def _calculate_procedural(
+        cov_type,
+        covden_sum,
+        covden_win,
+        hru_intcpstor,
+        hru_intcpevap,
+        hru_ppt,
+        hru_rain,
+        hru_snow,
+        intcp_changeover,
+        intcp_evap,
+        intcp_form,
+        intcp_stor,
+        intcp_transp_on,
+        net_ppt,
+        net_rain,
+        net_snow,
+        nhru,
+        pkwater_ante,
+        potet,
+        potet_sublim,
+        snow_intcp,
+        srain_intcp,
+        transp_on,
+        wrain_intcp,
+        time_length,
+        intercept,
+    ):
 
         # Assign short variables for input variables
-        hru_ppt = self.hru_ppt
-        potet = self.potet
-        hru_rain = self.hru_rain
-        hru_snow = self.hru_snow
-        intcp_form = self.intcp_form
-        transp_on = self.transp_on
+        hru_ppt = hru_ppt
+        potet = potet
+        hru_rain = hru_rain
+        hru_snow = hru_snow
+        intcp_form = intcp_form
+        transp_on = transp_on
 
         # set hrutype to LAND as this is only type supported in NHM
-        hru_type = np.array(self.nhru * [LAND])
+        hru_type = np.array(nhru * [LAND])
 
-        for i in range(self.nhru):
-            harea = self.hru_area[i]
+        for i in range(nhru):
             netrain = hru_rain[i]
             netsnow = hru_snow[i]
 
             if transp_on[i] == ACTIVE:
-                cov = self.covden_sum[i]
-                stor_max_rain = self.srain_intcp[i]
+                cov = covden_sum[i]
+                stor_max_rain = srain_intcp[i]
             else:
-                cov = self.covden_win[i]
-                stor_max_rain = self.wrain_intcp[i]
+                cov = covden_win[i]
+                stor_max_rain = wrain_intcp[i]
 
             intcp_form[i] = RAIN
             if hru_snow[i] > 0.0:
                 intcp_form[i] = SNOW
 
-            intcpstor = self.intcp_stor[i]
+            intcpstor = intcp_stor[i]
             intcpevap = 0.0
             changeover = 0.0
             extra_water = 0.0
 
             # lake or bare ground hrus
-            if hru_type[i] == LAKE or self.cov_type[i] == BARESOIL:
-                if self.cov_type[i] == BARESOIL and intcpstor > 0.0:
-                    extra_water = self.intcp_stor[i]
+            if hru_type[i] == LAKE or cov_type[i] == BARESOIL:
+                if cov_type[i] == BARESOIL and intcpstor > 0.0:
+                    extra_water = intcp_stor[i]
                 intcpstor = 0.0
 
             # ***** go from summer to winter cover density
-            if transp_on[i] == OFF and self.intcp_transp_on[i] == ACTIVE:
-                self.intcp_transp_on[i] = OFF
+            if transp_on[i] == OFF and intcp_transp_on[i] == ACTIVE:
+                intcp_transp_on[i] = OFF
                 if intcpstor > 0.0:
-                    diff = self.covden_sum[i] - cov
+                    diff = covden_sum[i] - cov
                     changeover = intcpstor * diff
                     if cov > 0.0:
                         if changeover < 0.0:
-                            intcpstor = intcpstor * self.covden_sum[i] / cov
+                            intcpstor = intcpstor * covden_sum[i] / cov
                             changeover = 0.0
                     else:
                         intcpstor = 0.0
 
             # ****** go from winter to summer cover density, excess = throughfall
-            elif transp_on[i] == ACTIVE and self.intcp_transp_on[i] == OFF:
-                self.intcp_transp_on[i] = ACTIVE
+            elif transp_on[i] == ACTIVE and intcp_transp_on[i] == OFF:
+                intcp_transp_on[i] = ACTIVE
                 if intcpstor > 0.0:
-                    diff = self.covden_win[i] - cov
+                    diff = covden_win[i] - cov
                     changeover = intcpstor * diff
                     if cov > 0.0:
                         if changeover < 0.0:
-                            intcpstor = intcpstor * self.covden_win[i] / cov
+                            intcpstor = intcpstor * covden_win[i] / cov
                             changeover = 0.0
                     else:
                         intcpstor = 0.0
 
             # *****Determine the amount of interception from rain
             # IF ( Hru_type(i)/=LAKE .AND. Cov_type(i)/=BARESOIL ) THEN
-            if hru_type[i] != LAKE and self.cov_type[i] != BARESOIL:
+            if hru_type[i] != LAKE and cov_type[i] != BARESOIL:
                 if hru_rain[i] > 0.0:
                     if cov > 0.0:
                         # IF ( Cov_type(i)>GRASSES ) THEN
-                        if self.cov_type[i] > GRASSES:
+                        if cov_type[i] > GRASSES:
                             # intercept(Hru_rain(i), stor_max_rain, cov, intcpstor, netrain)
-                            intcpstor, netrain = self.intercept(
+                            intcpstor, netrain = intercept(
                                 hru_rain[i],
                                 stor_max_rain,
                                 cov,
                                 intcpstor,
                                 netrain,
                             )
-                        elif self.cov_type[i] == GRASSES:
+                        elif cov_type[i] == GRASSES:
                             # if there is no snowpack and no snowfall, then apparently, grasses
                             # can intercept rain.
                             # IF ( pkwater_ante(i)<DNEARZERO .AND. netsnow<NEARZERO ) THEN
                             if (
-                                self.pkwater_ante[i] < DNEARZERO
+                                pkwater_ante[i] < DNEARZERO
                                 and netsnow < NEARZERO
                             ):
-                                intcpstor, netrain = self.intercept(
+                                intcpstor, netrain = intercept(
                                     hru_rain[i],
                                     stor_max_rain,
                                     cov,
@@ -247,10 +313,10 @@ class PRMSCanopy(StorageUnit):
             # ******Determine amount of interception from snow
             if hru_snow[i] > 0.0:
                 if cov > 0.0:
-                    if self.cov_type[i] > GRASSES:
-                        intcpstor, netsnow = self.intercept(
+                    if cov_type[i] > GRASSES:
+                        intcpstor, netsnow = intercept(
                             hru_snow[i],
-                            self.snow_intcp[i],
+                            snow_intcp[i],
                             cov,
                             intcpstor,
                             netsnow,
@@ -271,7 +337,7 @@ class PRMSCanopy(StorageUnit):
                 if hru_ppt[i] < NEARZERO:
                     epan_coef = 1.0
                     evrn = potet[i] / epan_coef
-                    evsn = potet[i] * self.potet_sublim[i]
+                    evsn = potet[i] * potet_sublim[i]
 
                     # todo: pan et
                     # IF ( Use_pandata==ACTIVE ) THEN
@@ -305,20 +371,31 @@ class PRMSCanopy(StorageUnit):
                 intcpstor = intcpstor + last - intcpevap
 
             # Store calculated values in output variables
-            self.intcp_evap[i] = intcpevap
-            self.intcp_stor[i] = intcpstor
-            self.net_rain[i] = netrain
-            self.net_snow[i] = netsnow
-            self.net_ppt[i] = netrain + netsnow
-            self.hru_intcpstor[i] = intcpstor * cov
-            self.hru_intcpevap[i] = intcpevap * cov
+            intcp_evap[i] = intcpevap
+            intcp_stor[i] = intcpstor
+            net_rain[i] = netrain
+            net_snow[i] = netsnow
+            net_ppt[i] = netrain + netsnow
+            hru_intcpstor[i] = intcpstor * cov
+            hru_intcpevap[i] = intcpevap * cov
 
-            self.intcp_changeover[i] = changeover + extra_water
+            intcp_changeover[i] = changeover + extra_water
 
-        return
+        return (
+            intcp_evap,
+            intcp_stor,
+            net_rain,
+            net_snow,
+            net_ppt,
+            hru_intcpstor,
+            hru_intcpevap,
+            intcp_changeover,
+            intcp_transp_on,
+        )
 
     @staticmethod
-    def intercept(precip, stor_max, cov, intcp_stor, net_precip):
+    @nb.jit(nopython=True)
+    def _intercept(precip, stor_max, cov, intcp_stor, net_precip):
         net_precip = precip * (1.0 - cov)
         intcp_stor = intcp_stor + precip
         if intcp_stor > stor_max:

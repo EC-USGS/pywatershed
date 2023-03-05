ecd8c809ed2e62042f73f024e9ecf12b769430dd
Wes Bonelli <wpb3hw@virginia.edu>
2023-03-04 22:44:43 -0500

    refactor test data generation

--- test_data/README.md
+++ test_data/README.md
@@ -1,9 +1,8 @@
-# test_data/
+# Test data
 
-This directory contains
+The `test_data` directory contains
   * domain directories for domain tests
-  * scripts/ which document how domain tests are established.
-
+  * `scripts/`, which contains scripts to run domain simulations and generate data.
 
 ## Domain directories
 
@@ -15,15 +14,18 @@ output to a disk faster than the one where the repository lives. However, we do 
 version control certain files in the directory. See the `conus_2yr/README.md` for details 
 on how it is setup.
 
-
 Other domain directories can be run from pytest given the repository. The pytest 
 flag `--all_domains` will detect these repos and run them.
 
-
-
-# scripts/
-
-This contains code for reproducing test data in the domains. Importantly, `run_domains.py` should be run
-occasionally to update the test data.
-
 Original source data can be found on Denali for most (all?) domains in `/home/jmccreight/pywatershed_data`.
+
+# Generating data
+
+The `test_data/scripts` subdirectory contains code for reproducing test data in the domains. Importantly, `test_run_domains.py` should be run occasionally to update the test data.  After running the domains, NetCDF files can be created from simulation outputs by running the tests in `test_nc_domains.py`. E.g.,
+
+```shell
+pytest -v -n auto test_run_domains.py
+pytest -v -n auto test_nc_domains.py
+```
+
+NetCDF dependencies are encoded implicitly into the `pytest` fixture system: `test_nc_domains.py` uses a custom test parametrization with `pytest_generate_tests` to map each CSV file created by the domain simulation to one or more NetCDF files, which are then aggregated into further files on session teardown by [yield fixtures](https://docs.pytest.org/en/7.2.x/how-to/fixtures.html#teardown-cleanup-aka-fixture-finalization). A [filelock](https://pytest-xdist.readthedocs.io/en/latest/how-to.html#making-session-scoped-fixtures-execute-only-once) is used to ensure aggregate files are only created once, even with multiple `pytest-xdist` workers.
--- test_data/scripts/test_nc_domains.py
+++ test_data/scripts/test_nc_domains.py
@@ -1,89 +1,113 @@
-from time import sleep
+from pathlib import Path
+from filelock import FileLock
 
 import numpy as np
+
+from pywatershed import CsvFile, Soltab
+from pywatershed.parameters import PrmsParameters
+from pywatershed.constants import epsilon64, zero
+
 import pytest
 import xarray as xr
 
-from pywatershed import CsvFile, Soltab
-from pywatershed.constants import epsilon64, zero
-from pywatershed.parameters import PrmsParameters
+
+# For generating timeseries of previous states
+previous_vars = [
+    "dprst_stor_hru",
+    "freeh2o",
+    "hru_impervstor",
+    "pk_ice",
+    "pref_flow_stor",
+    "slow_stor",
+    "soil_lower",
+    "soil_moist",
+    "soil_rechr",
+    "ssres_stor",
+]
+
+misc_nc_file_vars = [
+    "infil",
+    "sroff",
+    "ssres_flow",
+    "gwres_flow",
+]
 
 
-def test_csv_to_netcdf(csv_files):
-    nc_path = csv_files.with_suffix(".nc")
-    CsvFile(csv_files).to_netcdf(nc_path)
+final_nc_file_vars = [
+    "through_rain",
+]
+
+
+@pytest.fixture
+def netcdf_file(csv_file) -> Path:
+    """Convert CSV files from model output to NetCDF"""
+
+    nc_path = csv_file.with_suffix(".nc")
+    data_dir = csv_file.parent
+    domain_dir = data_dir.parent
+    CsvFile(csv_file).to_netcdf(nc_path)
     assert nc_path.exists()
 
+    if csv_file.stem in previous_vars:
+        nc_name = csv_file.stem + "_prev.nc"
+        nc_path = data_dir / nc_name
+        csv = CsvFile({nc_path.stem: csv_file})
+        csv._get_data()  # why so private?
+        orig_dates = csv._data["date"].copy()
+        csv._data = np.roll(csv._data, 1, axis=0)
+        csv._data["date"] = orig_dates
+        # Here we will eventually want to supply the desired initial conditions
+        for hh in range(len(csv._data[0])):
+            if hh == 0:
+                continue
+            csv._data[0][hh] = np.zeros([1])[0]
+        csv.to_netcdf(nc_path)
+        assert nc_path.exists()
 
-def test_soltab_to_netcdf(soltab_file):
-    # the nhm_ids are not available in the solta_debug file currently, so get
-    # them from the domain parameters
-    domain_dir = soltab_file.parent
-    # this is a hack that should probably rely on the yaml if/when this fails
-    params = PrmsParameters.load(domain_dir / "myparam.param")
-    nhm_ids = params.parameters["nhm_id"]
-
-    output_dir = domain_dir / "output"
-    soltab = Soltab(soltab_file, output_dir=output_dir, nhm_ids=nhm_ids)
-
-    for var in soltab.variables:
-        assert (output_dir / f"{var}.nc").exists()
-
-
-@pytest.mark.order(after=["test_csv_to_netcdf"])
-def test_csv_to_previous_netcdf(csv_files_prev):
-    nc_name = csv_files_prev.with_suffix("").name + "_prev.nc"
-    nc_path = csv_files_prev.parent / nc_name
-    csv = CsvFile({nc_path.stem: csv_files_prev})
-    csv._get_data()  # why so private?
-    orig_dates = csv._data["date"].copy()
-    csv._data = np.roll(csv._data, 1, axis=0)
-    csv._data["date"] = orig_dates
-    # Here we will eventually want to supply the desired initial conditions
-    for hh in range(len(csv._data[0])):
-        if hh == 0:
-            continue
-        csv._data[0][hh] = np.zeros([1])[0]
-    csv.to_netcdf(nc_path)
-    assert nc_path.exists()
-
-
-@pytest.mark.order(after=["test_csv_to_previous_netcdf"])
-def test_misc_netcdf(misc_nc_files_input):
-    if misc_nc_files_input.name in ["sroff", "ssres_flow", "gwres_flow"]:
-        data_dir = misc_nc_files_input.parent
-        domain_dir = misc_nc_files_input.parent.parent
+    if nc_path.stem in ["sroff", "ssres_flow", "gwres_flow"]:
         params = PrmsParameters.load(domain_dir / "myparam.param")
-        var = misc_nc_files_input.name
+        var = nc_path.stem
         ds = xr.open_dataset(data_dir / f"{var}.nc")
         ds = ds.rename({var: f"{var}_vol"})
-        ds = ds * params.data_vars["hru_in_to_cf"]
+        ds = ds * params.metadata["hru_in_to_cf"]
         ds.to_netcdf(data_dir / f"{var}_vol.nc")
         ds.close()
 
-    if misc_nc_files_input.name == "infil":
-        domain_dir = misc_nc_files_input.parent.parent
+    if nc_path.stem == "infil":
         params = PrmsParameters.load(domain_dir / "myparam.param").parameters
         imperv_frac = params["hru_percent_imperv"]
         dprst_frac = params["dprst_frac"]
         perv_frac = 1.0 - imperv_frac - dprst_frac
-        ds = xr.open_dataset(misc_nc_files_input.with_suffix(".nc"))
+        ds = xr.open_dataset(nc_path.with_suffix(".nc"))
         ds = ds.rename(infil="infil_hru")
-
         # not necessary
         # perv_frac = np.tile(perv_frac, (ds["infil_hru"].shape[0], 1))
         ds["infil_hru"] = ds["infil_hru"] * perv_frac
-
-        ds.to_netcdf(misc_nc_files_input.parent / "infil_hru.nc")
+        ds.to_netcdf(data_dir / "infil_hru.nc")
         ds.close()
 
-    assert True
+    return nc_path
 
 
-@pytest.mark.order(after=["test_misc_netcdf", "test_csv_to_previous_netcdf"])
-def test_misc_final(misc_nc_final_input):
-    if misc_nc_final_input.name == "through_rain":
-        data_dir = misc_nc_final_input.parent
+def make_netcdf_files(netcdf_file):
+    print(f"Created NetCDF from CSV: {netcdf_file}")
+
+
+@pytest.fixture(scope="session")
+def final_netcdf_file(tmp_path_factory, simulation) -> Path:
+    """Create the final NetCDF file (through_rain.nc) from other NetCDFs"""
+
+    root_tmpdir = tmp_path_factory.getbasetemp().parent
+    data_dir = Path(simulation["ws"]) / "output"
+    data_dir.mkdir(exist_ok=True)
+    nc_path = data_dir / "through_rain.nc"
+
+    with FileLock(root_tmpdir / "final_nc.lock"):
+        yield nc_path  # wait til session cleanup
+
+        if nc_path.is_file():
+            return
+
         data_vars = [
             "net_rain",
             "pk_ice_prev",
@@ -95,38 +119,57 @@ def test_misc_final(misc_nc_final_input):
         data = {}
         for vv in data_vars:
             data_file = data_dir / f"{vv}.nc"
-
-            if False:
-                result = xr.open_dataset(data_file)[vv]
-            else:
-                # None of this case should be necessary if the test
-                # really runs after the other tests as specified
-                # But the following will apparently trigger the race conditions
-                # rm ../*/output/*.nc; pytest -vv -n=auto test_nc_domains.py
-                result = None
-                while result is None:
-                    try:
-                        result = xr.open_dataset(data_file)[vv]
-                    except FileNotFoundError:
-                        sleep(0.1)
-                        pass
-                    except OSError:
-                        sleep(0.1)
-                        pass
-
+            result = xr.open_dataset(data_file)[vv]
             data[vv] = result
 
-        wh_through = (
-            ((data["pk_ice_prev"] + data["freeh2o_prev"]) <= epsilon64)
-            & ~(data["newsnow"] == 1)
-        ) | (data["pptmix_nopack"] == 1)
+        try:
+            wh_through = (
+                ((data["pk_ice_prev"] + data["freeh2o_prev"]) <= epsilon64)
+                & ~(data["newsnow"] == 1)
+            ) | (data["pptmix_nopack"] == 1)
 
-        through_rain = data["net_rain"].copy()
-        through_rain[:] = np.where(wh_through, data["net_rain"], zero)
+            through_rain = data["net_rain"].copy()
+            through_rain[:] = np.where(wh_through, data["net_rain"], zero)
+            through_rain.to_dataset(name="through_rain").to_netcdf(nc_path)
+            through_rain.close()
+            assert nc_path.exists()
+        finally:
+            for vv in data_vars:
+                data[vv].close()
 
-        through_rain.to_dataset(name="through_rain").to_netcdf(
-            misc_nc_final_input.parent / "through_rain.nc"
-        )
-        through_rain.close()
-        for vv in data_vars:
-            data[vv].close()
+@pytest.fixture(scope="session")
+def soltab_netcdf_file(tmp_path_factory, soltab_file) -> Path:
+    """Convert soltab files to NetCDF, one file for each variable"""
+
+    # the nhm_ids are not available in the solta_debug file currently, so get
+    # them from the domain parameters
+    domain_dir = soltab_file.parent
+    output_dir = domain_dir / "output"
+    output_dir.mkdir(exist_ok=True)
+    root_tmpdir = tmp_path_factory.getbasetemp().parent
+    nc_paths = []
+
+    with FileLock(root_tmpdir / "soltab_nc.lock"):
+        yield soltab_file  # wait til session cleanup
+
+        # this is a hack that should probably rely on the yaml if/when this fails
+        params = PrmsParameters.load(domain_dir / "myparam.param")
+        nhm_ids = params.parameters["nhm_id"]
+        soltab = Soltab(soltab_file, nhm_ids=nhm_ids)
+
+        already_created = (output_dir / "soltab_potsw.nc").is_file()
+        if not already_created:
+            soltab.to_netcdf(output_dir=output_dir)
+            print(f"Created NetCDF files from soltab file {soltab_file}:")
+
+        for var in soltab.variables:
+            nc_path = output_dir / f"{var}.nc" 
+            nc_paths.append(nc_path)
+            assert nc_path.exists()
+            print(f"\t{nc_path}")
+
+def make_soltab_netcdf_files(soltab_netcdf_file):
+    print(f"Creating NetCDF files for soltab file {soltab_netcdf_file}")
+
+def make_final_netcdf_files(final_netcdf_file):
+    print(f"Creating final NetCDF file {final_netcdf_file}")

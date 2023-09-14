132994b2c3b307a00563e3c5be821381c9cee53b
James McCreight <jamesmcc@ucar.edu>
2023-09-14 17:34:49 -0600

    fix csv removal

--- test_data/scripts/test_remove_csvs.py
+++ test_data/scripts/test_remove_csvs.py
@@ -1,9 +1,6 @@
-from .conftest import previous_vars
-
 csvs_to_keep = ["hru_ppt", "intcp_stor", "potet", "gwres_stor"]
 
 
 def test_csv_to_netcdf(csv_files):
-    print((csvs_to_keep + previous_vars))
-    if csv_files.with_suffix("").name not in (csvs_to_keep + previous_vars):
+    if csv_files.with_suffix("").name not in (csvs_to_keep):
         csv_files.unlink()

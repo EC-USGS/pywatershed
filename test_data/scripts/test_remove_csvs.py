53646742f8dedf25576028e88f7c0580389791b3
James McCreight <jamesmcc@ucar.edu>
2023-09-14 17:59:14 -0600

    fix ur fixture

--- test_data/scripts/test_remove_csvs.py
+++ test_data/scripts/test_remove_csvs.py
@@ -1,6 +1,6 @@
 csvs_to_keep = ["hru_ppt", "intcp_stor", "potet", "gwres_stor"]
 
 
-def test_csv_to_netcdf(csv_files):
-    if csv_files.with_suffix("").name not in (csvs_to_keep):
-        csv_files.unlink()
+def test_csv_to_netcdf(csv_file):
+    if csv_file.with_suffix("").name not in (csvs_to_keep):
+        csv_file.unlink()

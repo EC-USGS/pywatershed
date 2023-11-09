49b9103a4c69e4f20832c8d382dd6818d58c83ea
James McCreight <jamesmcc@ucar.edu>
2023-11-09 11:17:43 -0700

    cleanup reference dir

--- reference/dist_models/acf/gui.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-..\..\bin\prms -C.\control\acf.control -set print_debug -2 -print
-java -cp ..\..\dist\oui4.jar oui.mms.gui.Mms .\control\acf.control
-ECHO.
-ECHO Run complete. Please press enter to continue.
-PAUSE>NUL

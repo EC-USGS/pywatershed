6775308afeb8e3c6d5e80b5eb23251a7639d546b
James McCreight <jamesmcc@ucar.edu>
2024-06-28 19:57:59 -0600

    warn on optional import

--- pywatershed/plot/domain_plot.py
+++ pywatershed/plot/domain_plot.py
@@ -11,8 +11,8 @@ import pandas as pd
 from ..base import Parameters
 from ..utils import import_optional_dependency
 
-folium = import_optional_dependency("folium", errors=None)
-IPython = import_optional_dependency("IPython", errors=None)
+folium = import_optional_dependency("folium", errors="warn")
+IPython = import_optional_dependency("IPython", errors="warn")
 
 
 class DomainPlot:
